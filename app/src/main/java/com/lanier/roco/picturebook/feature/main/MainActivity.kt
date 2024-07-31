bded344f2d38ea6f225315ceb1e531ae2b7a4b90
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-07-31 16:01:40 +0800

    update: 颜色调整

--- app/src/main/java/com/lanier/roco/picturebook/feature/main/MainActivity.kt
+++ app/src/main/java/com/lanier/roco/picturebook/feature/main/MainActivity.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lanier.roco.picturebook.R
@@ -73,7 +76,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         setSupportActionBar(findViewById(R.id.toolbar))
 
