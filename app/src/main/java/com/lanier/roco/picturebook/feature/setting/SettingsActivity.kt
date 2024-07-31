9c95885de8dc7a673c48bc713d894bff0323d626
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-07-31 19:01:01 +0800

    update: 设置页面样式调整

--- app/src/main/java/com/lanier/roco/picturebook/feature/setting/SettingsActivity.kt
+++ app/src/main/java/com/lanier/roco/picturebook/feature/setting/SettingsActivity.kt
@@ -2,8 +2,11 @@ package com.lanier.roco.picturebook.feature.setting
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.lanier.roco.picturebook.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,7 +15,13 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         setSupportActionBar(findViewById(R.id.toolbar))
     }
