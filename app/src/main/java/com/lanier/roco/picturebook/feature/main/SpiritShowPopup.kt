5281711efa7c2d68f3096fe8c6698998bc12edd6
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-08-01 21:17:55 +0800

    调整为databinding形式

--- app/src/main/java/com/lanier/roco/picturebook/feature/main/SpiritShowPopup.kt
+++ app/src/main/java/com/lanier/roco/picturebook/feature/main/SpiritShowPopup.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.FragmentManager
-import coil.load
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.imageview.ShapeableImageView
-import com.lanier.roco.picturebook.R
 import com.lanier.roco.picturebook.database.entity.Spirit
+import com.lanier.roco.picturebook.databinding.PopupSpiritDetailsBinding
 import com.lanier.roco.picturebook.manager.AppData
 
 class SpiritShowPopup : BottomSheetDialogFragment() {
@@ -35,29 +32,22 @@ class SpiritShowPopup : BottomSheetDialogFragment() {
 
     private val spirit by lazy { arguments?.getParcelable("spirit") as? Spirit }
 
-    private lateinit var rootView: View
-    private lateinit var ivAvatar: ShapeableImageView
-    private lateinit var tvId: TextView
-    private lateinit var tvName: TextView
+    private val binding by lazy { PopupSpiritDetailsBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        rootView = inflater.inflate(R.layout.popup_spirit_details, container, false)
-        ivAvatar = rootView.findViewById(R.id.ivAvatar)
-        tvId = rootView.findViewById(R.id.tvID)
-        tvName = rootView.findViewById(R.id.tvName)
-        return rootView
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         spirit?.let {
-            AppData.loadSpiritAvatar(ivAvatar, it.iconSrc)
-            tvId.text = it.id
-            tvName.text = it.name
+            AppData.loadSpiritAvatar(binding.ivAvatar, it.iconSrc)
+            binding.tvID.text = it.id
+            binding.tvName.text = it.name
         }
     }
 }
