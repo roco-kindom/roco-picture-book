670473ea1bdea76be30e534c0fce4644f694945d
幻弦让叶 <1248389474@qq.com>
2024-10-07 16:53:19 +0800

    opt: image loading

--- app/src/main/java/com/lanier/roco/picturebook/BaseApplication.kt
+++ app/src/main/java/com/lanier/roco/picturebook/BaseApplication.kt
@@ -1,16 +1,31 @@
 package com.lanier.roco.picturebook
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
 import com.lanier.roco.picturebook.database.VioletDatabase
 import com.lanier.roco.picturebook.manager.sync.DbSyncManager
 import com.lanier.roco.picturebook.manager.SPManager
 
-class BaseApplication : Application() {
+class BaseApplication : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
         VioletDatabase.init(this)
         DbSyncManager.cachePath = externalCacheDir?.absolutePath?: ""
         SPManager.init(this)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .respectCacheHeaders(false)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("img_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
     }
 }
